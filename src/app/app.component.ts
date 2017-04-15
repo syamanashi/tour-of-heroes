63d2bdc7f2dbe2d0edce6dbb30dd5387a96114a0
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-04-15 10:25:53 -0400

    5. Services - Promise-based version of a data service - Service as a provider. - ngOnInit - mock data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './hero';
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -77,16 +65,34 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
-
+  providers: [
+    HeroService,
+  ],
 })
-export class AppComponent  { 
+export class AppComponent implements OnInit  { 
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  onSelect(hero: Hero) {
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  getHeroes(): void {
+    // Pass the callback function as an argument to the Promise's then() method:
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  // Synchronous (old):
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
   
 }
 
