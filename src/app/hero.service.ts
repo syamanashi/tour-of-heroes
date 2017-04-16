20a0e5bdf89ccae7eb70b213cc404dd6f645558d
Sutton Yamanashi <sutton.yamanashi@xpo.com>
2017-04-16 08:54:45 -0400

    7. HTTP: GETTING AND SAVING DATA - Add ability to add/create a hero with an Add button, add() click handler, and a create() method in the Hero Service.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,6 +10,7 @@ import { Hero } from './hero';
 export class HeroService {
 
   private heroesUrl: string = 'api/heroes'; // URL to web api
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
   constructor(private http: Http) { }
 
@@ -43,9 +44,6 @@ export class HeroService {
     // return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
   }
 
-
-
-  private headers = new Headers({ 'Content-Type': 'application/json' });
   update(hero: Hero): Promise<Hero> {
     const url = `${this.heroesUrl}/${hero.id}`;
     return this.http
@@ -55,6 +53,13 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
